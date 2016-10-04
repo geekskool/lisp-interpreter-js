33ecfdee2c82c63e8004572159b488129184e2cf
Arivanandan <uarivanandan@gmail.com>
2016-10-04 09:48:19 +0530

    Update ljs.js

--- ljs.js
+++ ljs.js
@@ -1,27 +1,27 @@
 function tokenize (programString) {
-  return programString.replace (/\(/g,' ( ').replace (/\)/g,' ) ').trim ().split (/\s+/)
+  return programString.replace(/\(/g,' ( ').replace(/\)/g,' ) ').trim().split(/\s+/)
 }
 
 function tokenHandler (token) {
   var float
-  if (!isNaN (float = parseFloat (token))) return float
+  if (!isNaN (float = parseFloat(token))) return float
   return token
 }
 
 function parse (tokens) {
-  token = tokens.shift ()
+  token = tokens.shift()
   if (token === '(') {
     var expr = []
     while (tokens[0] !== ')' && typeof tokens[0] !== 'undefined') {
-      expr.push (parse (tokens))
+      expr.push(parse(tokens))
     }
-    tokens.shift ()
+    tokens.shift()
     return expr
   }
   if (token == ')'){
-    throw new Error ('Syntax Error')
+    throw new Error('Syntax Error')
   }
-  return tokenHandler (token)
+  return tokenHandler(token)
 }
 
 function Env (params, args, outer) {
@@ -29,14 +29,14 @@ function Env (params, args, outer) {
   this.outer = outer
   this.find = function (name) {
     if (name in this.dict) { return this.dict[name] }
-    return outer.find (name)
+    return outer.find(name)
   }
   this.set = function (name, val) { this.dict[name] = val }
   if (params && args) {
     if (params instanceof Array) {
-      for (var i = 0; i < params.length; i++) this.set (params[i], parseInt (args[i]))
+      for (var i = 0; i < params.length; i++) this.set(params[i], parseInt(args[i]))
     }
-    this.set (params, parseInt (args))
+    this.set (params, parseInt(args))
   }
 }
 
@@ -74,44 +74,44 @@ function eval (x, env) {
   if (typeof x === 'number') { return x }
   if (typeof x === 'string') {
     try
-    { return env.find (x) }
-    catch (e) { return String (x) }
+    { return env.find(x) }
+    catch (e) { return String(x) }
   }
   if (!x instanceof Array) { return x }
-  if (x[0] === 'if') { return eval (x[1], env) ? eval (x[2], env) : eval (x[3], env) }
-  if (x[0] === 'define') { return env.set (x[1], eval (x[2], env)) }
-  if (x[0] === 'lambda') { return function (args) { return eval (x[2], new Env (x[1], args, env)) }}
-  if (x[0] === 'set!') { env.find (x[1]).set (x[1], eval (x[2], env)) }
+  if (x[0] === 'if') { return eval(x[1], env) ? eval(x[2], env) : eval(x[3], env) }
+  if (x[0] === 'define') { return env.set(x[1], eval(x[2], env)) }
+  if (x[0] === 'lambda') { return function (args) { return eval(x[2], new Env(x[1], args, env)) }}
+  if (x[0] === 'set!') { env.find (x[1]).set(x[1], eval(x[2], env)) }
   if (x[0] === 'quote') { return x[1] }
   if (x[0] === 'begin') {
     x.shift ()
-    var out = x.pop ()
+    var out = x.pop()
     x.map (function (token) {
-      eval (token, env)
+      eval(token, env)
     });
-    return eval (out, env)
+    return eval(out, env)
   }
-  var expr1 = typeof eval (x[1], env)
-  var expr2 = typeof eval (x[2], env)
+  var expr1 = typeof eval(x[1], env)
+  var expr2 = typeof eval(x[2], env)
   if (typeof x[2] === 'undefined') {
-    if (expr1 === 'function') return eval (x[0], env) (eval (x[1](), env))
-    return eval (x[0], env) (eval (x[1], env))
+    if (expr1 === 'function') return eval(x[0], env) (eval(x[1](), env))
+    return eval(x[0], env) (eval(x[1], env))
   }
-  if (expr1 === 'function' && expr2 === 'function') return eval (x[0], env) (eval (x[1], env) (), eval (x[2], env) ())
-  if (expr1 === 'function') return eval (x[0], env) (eval (x[1], env) (), eval (x[2], env))
-  if (expr2 === 'function') return eval (x[0], env) (eval (x[1], env), eval (x[2], env) ())
-  return eval (x[0], env) (eval (x[1], env), eval (x[2], env))
+  if (expr1 === 'function' && expr2 === 'function') return eval(x[0], env)(eval(x[1], env)(), eval(x[2], env)())
+  if (expr1 === 'function') return eval(x[0], env)(eval(x[1], env)(), eval(x[2], env))
+  if (expr2 === 'function') return eval(x[0], env)(eval(x[1], env), eval(x[2], env)())
+  return eval(x[0], env)(eval(x[1], env), eval(x[2], env))
 }
 
-const readline = require ('readline')
-const rl =  readline.createInterface ({
+const readline = require('readline')
+const rl =  readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 })
 
 rl.on ('line', (input) => {
-	input = input.trim ()
-	var solution = eval ((parse (tokenize (input))), env)
+	input = input.trim()
+	var solution = eval((parse(tokenize (input))), env)
 	if (typeof solution === 'undefined') {}
 	else console.log(solution)
 })
